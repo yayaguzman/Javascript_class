d308cd29489d948585749268480b590fd96feed5
Yaya <cryayo@hotmail.com>
2020-03-08 11:14:45 -0500

    class 1 added

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-console.log("hello from javascript");
-console.log("hello");
-console.log(3*4+5);
+/*class 1- 
+Data types:
+Number: " " or ' ' if both, needs: "He said: \"Hello\"" escaped with \ 
+String: + - floating point 4.6
+Boolean: true or false
+Undefined: undefined
+\n: new line
+Open console on Chrome: alt+cmd+j= console
+*/
+"Yadira"
+39
+true
+
+/*Operators	
+Numerical + - * /	5 + 4 * 3   "Hello" + " World" only used with String and numbers
+
+Comparison 	=== !== < > <= >=	temperature !== 25   age >= 18
+=== strict equality  !== strict inequality  > greater than < less than  >= greater or equal <= less or equal
+
+Logical 	|| && !	            a && !b     x >= 5 && x < 15 only used with Boolean
+&& Logical AND (returns true if both booleans are true)
+|| Logical OR  (returns true if at least one is true)
+!  Logical NOT (negates a boolean  !true  // false   !false // true)
+
+Combined 	+= -= *= /= ++      a *= 2 count++
+*/
+
+39 + 1 //I'll be 40 next year/
+8 **2 //square of 8 is 64/
+42 < 20 && 42 < 35  // false - I am not the youngest person
+42 < 20 || 42 < 35  // false - I am not younger than at least one other person
+
