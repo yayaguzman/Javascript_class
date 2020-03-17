52e7783ad0919c2593a34db6cc4dba105f7826e4
Yaya <cryayo@hotmail.com>
2020-03-17 02:34:29 +0100

    class 4 added

--- main.js
+++ main.js
@@ -431,6 +431,7 @@ if (temperature > 30) {
 
 // class 4- 
 //Functions
+//Set of statments that perform a task or calculate a value
 /*
 EXERCISE
 Simple program that calculates the bonus an employee will get at the end of the year.
@@ -470,8 +471,13 @@ name of the function
 (param1, param2, ...) parameters are just like variables
 { ... } the block where we have our code
 we might add a return at the end, depending on what we need.
+*/
 
-*/function sayHello() {
+
+
+/*
+function sayHello() { 
+  //the block where we have our code
   console.log("Hello There!");
 
   console.log("...");
@@ -480,4 +486,108 @@ we might add a return at the end, depending on what we need.
 }
 
 sayHello();
+
+
+Calling a function
+function sayHello(){
+  console.log("Hello!");
+}
+sayHello();
+The function sayHello does not require any parameters that is why the parenthesis () are empty.
+
+
+function greet(name){
+  console.log("Hello " + name + " !");
+}
+greet("Bob");
+// "Hello Bob !"
+
+The function greet takes one parameter: name 
+that is why we write "Bob" in the parenthesis. we are giving the value "Bob" to the function.
+
+EXERCISE
+Write a function called celsiusToFahrenheit, which takes the degrees in C, and returns it in F.
+test your function with the values 28 and 31.
+Fahrenheit = Celsius multiplied by 1.8 plus 32
+
+before
+let degreesInCelsius= 28;
+// let degreesInFahrenheit= ???; Fahrenheit = Celsius multiplied by 1.8 plus 32
+let degreesInFahrenheit = degreesInCelsius * 1.8 + 32; 
+console.log("28 degrees Celsius= " + degreesInFahrenheit + " degrees Fahrenheit");
+*/
+
+function celsiusToFahrenheit(degreesCelsius) {
+  let degreesFahrenheit = degreesCelsius * 1.8 + 32;
+  return degreesFahrenheit;
+}
+
+console.log("28 degrees: " + celsiusToFahrenheit(28));
+console.log("32 degrees: " + celsiusToFahrenheit(32));
+
+/*
+Exercise
+Write a min function that returns the smaller of the two arguments passed to it: I DID NOT UNDERSTAND IT
+
+min(1, 2);    // should return 1
+min(100, 99); // should return 99
+min(-10, 0);  // should return -10
+*/
+
+
+//Homework 1
+//Write a function that takes name of a person, their age, and the language they speak, and returns a string that introduces this person.
+
+//performing a task
+function hola(theName, theAge, theLanguage) {
+  console.log("Hola my name is " + theName + ", I'm " + theAge + " years old " + "and I speak " + theLanguage);
+}
+
+hola("Claire", 23, "Spanish"); //Hola my name is Claire, I'm 23 years old and I speak Spanish
+
+/*Homework 2
+Change the employee solution from before, and turn it into a function.
+The function should accept the number of normal days and number of extra days as parameters!
+let regularDay = 1;
+let extraDay = 3;
+let employeeBonus = (regularDay * 200) + (extraDay * 20);
+
+if (employeeBonus < 100) {
+  console.log("Sorry, you do not get any bonus");
+} else if (employeeBonus > 250) {
+  console.log("Great, you get a pay raise with!! " + employeeBonus + " " + "bonus" );
+} else{
+  console.log("You get a chocolate box");
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Object literal
+let person {
+  name: "Jean",     //this are keys or properties this object has 2 keys
+  age: 34
+};
+console.log(person);
+
+//dot notation 
+person.name = 'John';
+
+//bracket notation "when we dont know the name and useris provinding it"
+let selecrtion = 'name';
+person['name'] ='Mary';
+console.log(person.name);
+*/
+
 
